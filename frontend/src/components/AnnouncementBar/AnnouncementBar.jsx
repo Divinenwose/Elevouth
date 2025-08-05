9808fd3e2d6515fba5fd736b2f9b937f9b4ecb49
Divine nwose <nwosedivine40@gmail.com>
2025-08-05 16:22:16 +0100

    updated components

--- frontend/src/components/AnnouncementBar/AnnouncementBar.jsx
+++ frontend/src/components/AnnouncementBar/AnnouncementBar.jsx
@@ -6,7 +6,7 @@ const AnnouncementBar = () => {
   return (
     <div className="announcement-bar">
       <div className="scroll-text">
-        {[...Array(15)].map((_, i) => (
+        {[...Array(50)].map((_, i) => (
           <div className="scroll-item" key={i}>
             <img src={smiley} alt="smiley" />
             <h2>Elevouth Global Summit 2025.</h2> 
