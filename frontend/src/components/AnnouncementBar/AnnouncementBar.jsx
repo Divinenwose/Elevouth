10b625ecd21d02e6af163826012aea6c63dc3a3e
Divine nwose <nwosedivine40@gmail.com>
2025-08-05 16:28:15 +0100

    updated component

--- frontend/src/components/AnnouncementBar/AnnouncementBar.jsx
+++ frontend/src/components/AnnouncementBar/AnnouncementBar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './AnnouncementBar.css';
-import smiley from  "../../assets/smiley.png";
+import smiley from "../../assets/smiley.png";
 
 const AnnouncementBar = () => {
   return (
     <div className="announcement-bar">
       <div className="scroll-text">
         {[...Array(50)].map((_, i) => (
-          <div className="scroll-item" key={i}>
+          <a
+            key={i}
+            href="https://lu.ma/ig51d4z2" 
+            className="scroll-item"
+            target="_blank" 
+            rel="noopener noreferrer" 
+          >
             <img src={smiley} alt="smiley" />
             <h2>Elevouth Global Summit 2025.</h2> 
             <p>Reserve your spot today - limited access.</p>
-          </div>
+          </a>
         ))}
       </div>
     </div>
